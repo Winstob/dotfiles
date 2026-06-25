43dd699d86b8a236136ee3f412250aef9238a1ca
Winstob <gavin@eralston.net>
2026-06-25 11:39:24 -0400

    Change git blame date format

--- nvim/lua/git-blame/init.lua
+++ nvim/lua/git-blame/init.lua
@@ -1,5 +1,5 @@
 require('gitblame').setup({
     enabled = true,
-    date_format = " %Y-%m-%d %H:%M",
+    date_format = "%Y-%m-%d %H:%M",
     message_template = "  • <author> • <date> • <summary>",
 })
