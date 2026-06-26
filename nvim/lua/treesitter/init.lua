d88e2e2bf187203f402edaa54e83432d6a45a1e7
Winstob <gavin@eralston.net>
2026-06-26 11:33:36 -0400

    Add markdown to treesitter

--- nvim/lua/treesitter/init.lua
+++ nvim/lua/treesitter/init.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter').setup {
-    ensure_installed = { "rust", "lua", "toml", "json", "c", "c++" },
+    ensure_installed = { "rust", "lua", "toml", "json", "c", "c++", "markdown" },
+    auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
